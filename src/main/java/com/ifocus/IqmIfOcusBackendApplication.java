afb19ad8435b59cb02335e282cc4b84ef6a825f5
siba <sibaprasad.nayak@ifocussystec.com>
2017-05-09 17:21:36 +0530

    Have added stuff related to deploying war file in external  tomcat server.

--- src/main/java/com/ifocus/IqmIfOcusBackendApplication.java
+++ src/main/java/com/ifocus/IqmIfOcusBackendApplication.java
@@ -2,12 +2,30 @@ package com.ifocus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class IqmIfOcusBackendApplication {
+@RestController
+public class IqmIfOcusBackendApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(IqmIfOcusBackendApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IqmIfOcusBackendApplication.class, args);
+	}
+	
+	//Endpoint for testing spring boot
+	//TODO:it Can be remove later.
+	@RequestMapping(value="/")
+	public String testBoot(){
+		return "!!.....Welcome to IQM Tool...!!";
+		
 	}
 
 }
